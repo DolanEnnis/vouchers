8fd4a7c1652b12afb3fc280ee9ab5e77557edae5
Brian Dolan <dolan.ennis@gmail.com>
2022-04-14 13:34:15 +0100

    Added firebase

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: 'AIzaSyACOSpP3NCAJMXbZEYvJ6SQ4yhC54z6xUE',
+	authDomain: 'todos-f4a3d.firebaseapp.com',
+	projectId: 'todos-f4a3d',
+	storageBucket: 'todos-f4a3d.appspot.com',
+	messagingSenderId: '517964152241',
+	appId: '1:517964152241:web:649048fead71790bdcd190'
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,27 @@
 <script>
 	//import About from './about.svelte';
-	//import { initializeApp, getApps, getApp } from 'firebase/app';
-	//import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
-	//import { firebaseConfig } from '$lib/firebaseConfig';
+	import { initializeApp, getApps, getApp } from 'firebase/app';
+	import { getFirestore, collection, onSnapshot } from 'firebase/firestore';
+	import { firebaseConfig } from '$lib/firebaseConfig';
 	import { browser } from '$app/env';
 
+	const firebaseApp =
+		browser && (getApps().length === 0 ? initializeApp(firebaseConfig) : getApp());
+
+	const db = browser && getFirestore();
 	let todos = [];
+
+	const colRef = browser && collection(db, 'todos');
+	const unsubscribe =
+		browser &&
+		onSnapshot(colRef, (querySnapshot) => {
+			let fbTodos = [];
+			querySnapshot.forEach((doc) => {
+				let todo = { ...doc.data(), id: doc.id };
+				fbTodos = [todo, ...fbTodos];
+			});
+			todos = fbTodos;
+		});
 
 	let task = '';
 
